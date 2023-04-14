cc8b6a849d9fdd0aebea30266e218187627b0b52
Sunish Sheth <sunishsheth2009@gmail.com>
2023-04-14 14:30:47 -0700

    Updating post_split_filter_method to be only called in training dataset
    Corresponding MLflow PR: https://github.com/mlflow/mlflow/pull/8230

--- steps/split.py
+++ steps/split.py
@@ -1,8 +1,8 @@
 """
 This module defines the following routines used by the 'split' step of the regression recipe:
 
-- ``create_dataset_filter``: Defines customizable logic for filtering the training, validation,
-  and test datasets produced by the data splitting procedure. Note that arbitrary transformations
+- ``create_dataset_filter``: Defines customizable logic for filtering the training
+  datasets produced by the data splitting procedure. Note that arbitrary transformations
   should go into the transform step.
 """
 
@@ -12,7 +12,7 @@ from pandas import DataFrame, Series
 def create_dataset_filter(dataset: DataFrame) -> Series(bool):
     """
     Mark rows of the split datasets to be additionally filtered. This function will be called on
-    the training, validation, and test datasets.
+    the training datasets.
 
     :param dataset: The {train,validation,test} dataset produced by the data splitting procedure.
     :return: A Series indicating whether each row should be filtered
